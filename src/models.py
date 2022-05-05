cc509f219959a9d81a05204076eec410acdbd13b
jsowers13 <jsowers13@gmail.com>
2022-05-05 17:45:28 +0000

    completed project

--- src/models.py
+++ src/models.py
@@ -24,13 +24,63 @@ class People(Base):
     gender = Column(String(250), nullable=False)
     home_world_id = Column(String(250), ForeignKey('planets.id'), nullable=False)
     home_world = relationship("planets")
-    vehicle_id = Column(String(250), ForeignKey('vehicles.id'), nullable=False)
-    vehicle = relationship('vehicles')
+    starship_id = Column(String(250), ForeignKey('starships.id'), nullable=False)
+    starship = relationship('starships')
+    favorites_id = Column(String(250), ForeignKey('favorites.id'), nullable=False)
+    favorites = relationship('favorites')
 
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    diameter = Column(Integer, nullable=False)
+    climate = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    surface_water = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=False)
+    residents_id = Column(String(250), ForeignKey('people.id'), nullable=False)
+    residents = relationship('people')
+    favorites_id = Column(String(250), ForeignKey('favorites.id'), nullable=False)
+    favorites = relationship('favorites')
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cost_in_credits = Column(Integer, nullable=False)
+    length = Column(Integer, nullable=False)
+    max_atmosphering_speed = Column(Integer, nullable=False)
+    crew_size = Column(Integer, nullable=False)
+    passengers = Column(Integer, nullable=False)
+    cargo_capacity = Column(Integer, nullable=False)
+    consumables = Column(String(250), nullable=False)
+    hyperdrive_rating = Column(String(250), nullable=False)
+    mglt = Column(Integer, nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    pilot_id = Column(String(250), ForeignKey('people.id'), nullable=False)
+    pilot = relationship('people')
+    favorites_id = Column(String(250), ForeignKey('favorites.id'), nullable=False)
+    favorites = relationship('favorites')
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(250), nullable=False)
+    favorites_id = Column(String(250), ForeignKey('favorites.id'), nullable=False)
+    favorites = relationship('favorites')
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(250), ForeignKey('user.id'), nullable=False)
+    user = relationship('user')
+
+
+
 
 
 # class Address(Base):
